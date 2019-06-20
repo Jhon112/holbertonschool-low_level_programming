926287cc50659f23add489ce0d4bfb1e34ac41da
Jhon112 <jhonarmont@hotmail.com>
2019-06-20 17:36:48 +0000

    Fix description and add return value

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 /**
-Main will print a string according to the value of n 
-**/
-/* betty style doc for function main goes there */
+ * main - Will print a string according to the value of n
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if (n == 0)
 printf("%d is zero\n", n);
 if (n > 0)
 printf("%d is positive\n", n);
 if (n < 0)
 printf("%d is negative\n", n);
-/* your code goes there */
+
 return (0);
 }
