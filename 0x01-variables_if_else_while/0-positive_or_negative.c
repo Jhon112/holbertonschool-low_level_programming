7c245c200c4b7f14032efd619d762366e44c7e91
Jhon112 <jhonarmont@hotmail.com>
2019-06-20 17:30:27 +0000

    Add description

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
+/**
+Main will print a string according to the value of n 
+**/
 /* betty style doc for function main goes there */
 int main(void)
 {
