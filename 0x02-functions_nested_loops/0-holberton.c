e2f28e55977dcb6e05f5a5a9666106685f4676aa
Jhon112 <jhonarmont@hotmail.com>
2019-06-25 15:52:41 +0000

    Create header file and call _putchar function, Add task 0 and README

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+char s[] = "Holberton";
+
+for (a = 0; a <= 8; a++)
+{
+_putchar(s[a]);
+}
+_putchar('\n');
+return (0);
+}
