f0077d9612af25e2368fb3a69354c13d079900f5
Jhon112 <jhonarmont@hotmail.com>
2019-06-27 22:00:38 +0000

    Fix betty error

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,13 +7,13 @@
  */
 void print_square(int n)
 {
-	int a,b;
+	int a, b;
 
 	if (n > 0)
 	{
 		for (a = 0; a < n; a++)
 		{
-			for(b = 0; b < n; b++)
+			for (b = 0; b < n; b++)
 				{
 					_putchar(35);
 				}
