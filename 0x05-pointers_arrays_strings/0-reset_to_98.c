1854e871cf140729346432940ef73cc96f95a8c7
Jhon112 <jhonarmont@hotmail.com>
2019-07-02 19:49:10 +0000

    Fix description. Add ':'

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the value
- * @n pointer to be updated
+ * @n: pointer to be updated
  *
  * Return: void
  */
