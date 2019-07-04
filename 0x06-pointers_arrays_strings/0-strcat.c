7643e599ed9fb017e919f1ff91469354f7915132
Jhon112 <jhonarmont@hotmail.com>
2019-07-04 17:14:02 +0000

    Task 0 - Concatenate an array

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest - pointer to dest string
+ * @src - pointer to src string(the one to append)
+ *
+ * Return: pointer to concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int position;
+
+	i = 0;
+	position = 0;
+	while (*(dest + i) != 0)
+	{
+		position++;
+		i++;
+	}
+	i = 0;
+	while (*(src + i) != 0)
+	{
+		dest[position] = *(src + i);
+		position++;
+		i++;
+	}
+	return (dest);
+}
