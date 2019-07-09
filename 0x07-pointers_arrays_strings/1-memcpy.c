bf6d8e798761222b2670ad4e56d0902a09c7c9e9
Jhon112 <jhonarmont@hotmail.com>
2019-07-09 04:08:15 +0000

    Edit value without increasing the address

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while( i < n)
+	while (i < n)
 	{
-		*dest = *src;
-		dest++;
+		*(dest + i) = *src;
 		src++;
 		i++;
 	}
