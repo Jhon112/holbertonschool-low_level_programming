5c2b4793b191ef5e04aa45e2e4042843b50f3db9
Jhon112 <jhonarmont@hotmail.com>
2019-07-09 04:32:29 +0000

    Write a function that prints the chessboard.
    Prototype: void print_chessboard(char (*a)[8]);

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * print_chessboard - Write a function that prints the chessboard.
+ * @a: multidimensional array
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int column;
+
+	row = 0;
+	while (row < 8)
+	{
+		if (row > 1 && row < 6)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			column = 0;
+			while (column < 8)
+			{
+				_putchar(a[row][column]);
+				column++;
+			}
+			_putchar('\n');
+		}
+		row++;
+	}
+}
