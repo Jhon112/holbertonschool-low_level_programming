5528714be773b7dfe44afc45a444bcc8a4a340d7
Jhon112 <jhonarmont@hotmail.com>
2019-07-15 04:13:36 +0000

    Write a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Calculate the lenght of a string
+ * @argc: The count of arguments
+ * @argv: An array containing the program command arguments
+ *
+ * Return: On success 1.
+ * On error, -1 is returned
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
