cbb61e0216920a7a86815baedce574ee5bc2eae7
Jhon112 <jhonarmont@hotmail.com>
2019-07-18 21:50:32 +0000

    Fix multiplication

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	bytes = nmemb + size;
+	bytes = nmemb * size;
 	p = malloc(bytes);
 	if (p == NULL)
 		return (NULL);
