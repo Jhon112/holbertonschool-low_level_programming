6dc481051d2371d73207732769b1f3c8a059cae9
Jhon112 <jhonarmont@hotmail.com>
2019-07-22 22:56:03 +0000

    Define a new type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+# README FOR 0x0E. C - Structures, typedef PROJECT
+
+
+### General
+What are structures, when, why and how to use them
+How to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a structure who defines dog object
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Dog object
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
