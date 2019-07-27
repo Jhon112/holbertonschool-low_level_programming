09fdb2556a4bc837d9cb3466c0ad3ea5551be8f1
Jhon112 <jhonarmont@hotmail.com>
2019-07-27 19:22:08 +0000

    fix betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,8 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(nums, int));
 		if (i + 1 != n)
 		{
-			if(separator != NULL)
+			if (separator != NULL)
+			{
 				printf("%s", separator);
+			}
 		}
 		else
 		{
