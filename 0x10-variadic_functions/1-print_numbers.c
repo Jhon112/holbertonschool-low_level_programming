0d4afa3430d03f3714e7da9775c417d38a4db401
Jhon112 <jhonarmont@hotmail.com>
2019-07-27 19:14:54 +0000

    Add condition inside while to avoid printing separator when null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,19 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list nums;
 
 	i = 0;
-
-	if (separator == NULL)
-		separator = "";
 	va_start(nums, n);
 	while (i < n)
 	{
 		printf("%d", va_arg(nums, int));
 		if (i + 1 != n)
-			printf("%s", separator);
+		{
+			if(separator != NULL)
+				printf("%s", separator);
+		}
 		else
+		{
 			printf("\n");
+		}
 		i++;
 	}
 	va_end(nums);
