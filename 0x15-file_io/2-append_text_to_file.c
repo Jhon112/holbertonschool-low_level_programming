711c206972fc70277ea60014a1fedea905c711b6
jhon112 <jhonarmont@hotmail.com>
2019-08-13 23:23:01 +0000

    Write a function that appends text at the end of a file.
    Prototype: int append_text_to_file(const char *filename, char *text_content);
    where filename is the name of the file and text_content is the NULL terminated string to add at the end of the file
    Return: 1 on success and -1 on failure
    Do not create the file if it does not exist
    If filename is NULL return -1
    If text_content is NULL, do not add anything to the file. Return 1 if the file exists and -1 if the file does not exist or if you do not have the required permissions to write the file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+/**
+ * get_length - returns lengt
+ * @str: str to be evaluated
+ *
+ * Return: length of str
+ */
+int get_length(char *str)
+{
+        int len;
+
+        for (len = 0; str[len] != '\0'; len++)
+                continue;
+        return (len);
+}
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of file
+ * @text_content: pointer to text to append
+ *
+ * Return: 1 if file exists, -1 otherwise
+ */
+int append_text_to_file(char *filename, char *text_content)
+{
+	int fd;
+	int len;
+
+	if (filename == 0)
+		return (-1);
+	if (text_content == 0)
+		text_content = "";
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		if (errno == ENOENT || errno == EROFS)
+			return (-1);
+	len = get_length(text_content);
+	if (write(fd, text_content, len) == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
