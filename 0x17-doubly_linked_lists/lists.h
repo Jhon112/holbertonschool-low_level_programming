e78b2ac4121518b0f1a058a40a8fac7c04e7d191
Jhon112 <jhonarmont@hotmail.com>
2019-09-15 07:29:20 +0000

    add header file with new prototypes

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,5 +27,7 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *create_node();
+dlistint_t *insert_node(dlistint_t *next, dlistint_t *current);
 
 #endif
