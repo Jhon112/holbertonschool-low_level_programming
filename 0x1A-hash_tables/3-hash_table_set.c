38898dea8b494a707d81119ad2a734a140495847
jhon112 <jhonarmont@hotmail.com>
2019-10-11 22:04:30 +0000

    Update value in case key already exist on list

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,8 +32,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	head = ht->array[index];
 	if (head)
+	{
 		new->next = head;
-
+		while (head != NULL)
+		{
+			if (strcmp(head->key, key_cpy) == 0)
+				head->value = value_cpy;
+			head = head->next;
+		}
+	}
 	head = new;
 
 	ht->array[index] = head;
