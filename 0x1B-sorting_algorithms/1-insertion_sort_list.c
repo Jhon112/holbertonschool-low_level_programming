9f9f53bd8691f1e3a6d4ad42fbef50f06478e67c
Jhon Arias <jhonarmont@hotmail.com>
2019-10-29 05:09:44 -0500

    Add task 1 which is an implementation of an insertion sort algortihm on a doubly linked list. It iterates all the way through the end of the list and once a previous element is greater than the current one, it iterates all the way through the first node of the list while sapping the nodes. Add file 1-O which contains the anwers to the big O notations of the time complexity of the Insertion sort algorithm, with 1 notation per line:
    in the best case
    in the average case
    in the worst case

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list in asc order using
+ * insertion sort algorithm and prints the list each time a swap is done
+ *
+ * @list: pointer to pointer with the address of first node
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp = NULL, *current = NULL, *previous = NULL;
+
+	temp = *list;
+	while (temp != NULL)
+	{
+		current = temp;
+		previous = current->prev;
+		while (current->prev != NULL && previous->n > current->n)
+		{
+			/*swap nodes*/
+
+			previous->next = current->next;
+			current->prev = previous->prev;
+			current->next = previous;
+			previous->prev = current;
+			if (previous->next)
+				previous->next->prev = previous;
+			if (current->prev)
+				current->prev->next = current;
+			else
+				*list = current;
+
+			print_list(*list);
+			previous = current->prev;
+		}
+		temp = temp->next;
+	}
+}
