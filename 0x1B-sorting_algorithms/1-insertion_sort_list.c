776585693f2dd869fb70eee87fa5ccc66c966cde
Jhon Arias <jhonarmont@hotmail.com>
2019-10-29 05:35:29 -0500

    Add validation to check address of list is not NULL. Add new line to 1-O file!

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -10,29 +10,32 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp = NULL, *current = NULL, *previous = NULL;
 
-	temp = *list;
-	while (temp != NULL)
+	if (list != NULL)
 	{
-		current = temp;
-		previous = current->prev;
-		while (current->prev != NULL && previous->n > current->n)
+		temp = *list;
+		while (temp != NULL)
 		{
-			/*swap nodes*/
-
-			previous->next = current->next;
-			current->prev = previous->prev;
-			current->next = previous;
-			previous->prev = current;
-			if (previous->next)
-				previous->next->prev = previous;
-			if (current->prev)
-				current->prev->next = current;
-			else
-				*list = current;
-
-			print_list(*list);
+			current = temp;
 			previous = current->prev;
+			while (current->prev != NULL && previous->n > current->n)
+			{
+				/*swap nodes*/
+
+				previous->next = current->next;
+				current->prev = previous->prev;
+				current->next = previous;
+				previous->prev = current;
+				if (previous->next)
+					previous->next->prev = previous;
+				if (current->prev)
+					current->prev->next = current;
+				else
+					*list = current;
+
+				print_list(*list);
+				previous = current->prev;
+			}
+			temp = temp->next;
 		}
-		temp = temp->next;
 	}
 }
