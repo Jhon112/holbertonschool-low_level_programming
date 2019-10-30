c96c379316cb59be3b4bfadfa5b09776b7897ef5
jhon112 <jhonarmont@hotmail.com>
2019-10-30 23:54:04 +0000

    add validations to only print when i and j does not value the same as it's not necessary

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,34 +1,45 @@
 #include "sort.h"
 
 /**
- * partition - it finds the index where the pivot should be.
- * groups less values at the left, and greater values at the right
+ * partition - place the pivot in its right index, then
+ * apply divide and conquer and calls itself with the left array
+ * and right array
  *
  * @array: array to be quick sorted
- * @end: right limit of array
- *
- * Return: index where the pivot was inserted
+ * @start: start of array to be sorted
+ * @end: end of array to be sorted
+ * @size: total size of original array
  */
 
-size_t partition(int *array, size_t end)
+void partition(int *array, int start, int end, size_t size)
 {
-	int pivot = array[end - 1], temp;
-	size_t pindex = 0, i;
+	int pivot = array[end], temp;
+	int i = start, j;
 
-	for (i = 0; i < end; i++)
+	if (start < end)
 	{
-		if (array[i] < pivot)
+		for (j = start; j <= end; j++)
 		{
-			temp = array[pindex];
-			array[pindex] = array[i];
-			array[i] = temp;
-			pindex++;
+			if (array[j] < pivot)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				if (i != j)
+					print_array(array, size);
+				i++;
+			}
 		}
+		if (array[i] != pivot)
+		{
+			temp = array[i];
+			array[i] = pivot;
+			array[end] = temp;
+			print_array(array, size);
+		}
+		partition(array, start, i - 1, size);
+		partition(array, i + 1, end, size);
 	}
-	temp = array[pindex];
-	array[pindex] = pivot;
-	array[end - 1] = temp;
-	return pindex;
 }
 
 /**
@@ -41,12 +52,8 @@ size_t partition(int *array, size_t end)
 
 void quick_sort(int *array, size_t size)
 {
-	size_t pindex = 0;
-
-	if (size > 2)
+	if (size > 2 && array != NULL)
 	{
-		pindex = partition(array, size);
-		quick_sort(array, pindex);
-		quick_sort((array + (pindex + 1)), size - (pindex + 1));
+		partition(array, 0, size - 1, size);
 	}
 }
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -21,6 +21,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
-size_t partition(int *array, size_t end);
+void partition(int *array, int start, int end, size_t size);
 
 #endif
