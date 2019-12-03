cd9d7b50d6e55ba7be805a07b7340a0c52e0ab36
jhon112 <jhonarmont@hotmail.com>
2019-12-03 18:56:04 +0000

    Add header with prototypes. ADd task 0 to create a binary tree node

---
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stdio.h>
+/**
+ * creates a binary tree
+ * @parent: pointer to a parent node
+ * @value: value to put in new node
+ *
+ * Return: pointer to new node, null otherwise
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = NULL;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	new->n = value;
+
+	return (new);
+}
