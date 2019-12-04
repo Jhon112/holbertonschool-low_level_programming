1e6f1a16bcdeba32b76a0337ed9479a29203758e
Jhon Arias <jhonarmont@hotmail.com>
2019-12-04 14:45:15 -0500

    Separate conditions

--- 0x1D-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -8,8 +8,10 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	if (!node)
+		return (0);
 
-	if (node->left || node->right || !node || !node->parent)
+	if (node->left || node->right)
 		return (0);
 	return (1);
 }
