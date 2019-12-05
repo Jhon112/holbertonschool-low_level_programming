2283103aa9831f197169fc11675a5544518e7a20
jhon112 <jhonarmont@hotmail.com>
2019-12-05 21:41:17 +0000

    Use post-order to traversal binary tree. left-right-node

---
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - goes through a binary tree
+ * using postorder-order
+ * @tree: pointer to the root node of tree
+ * @func: function to call for each node
+ *
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
