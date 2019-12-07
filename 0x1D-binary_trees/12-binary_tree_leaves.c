151b3399bb0c58a208f7a28fda52866ee3596184
Jhon Arias <jhonarmont@hotmail.com>
2019-12-07 01:22:19 -0500

    Fix betty

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -7,13 +7,13 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t leaves = 1;
+	size_t leaves = 1;
 
-    if (tree == NULL)
-        return (0);
+		if (tree == NULL)
+			return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
-    leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
-    return (leaves);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (leaves);
 }
