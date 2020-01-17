1be22d7216f183903e3bb3f26e32d0c4d82835f5
Jhon Arias <jhonarmont@hotmail.com>
2020-01-16 23:15:00 -0500

    Try different approach

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,26 +1,27 @@
 #include "search_algos.h"
 /**
- * linear_search - searches for a value in an array
- * @array: pointer to first element of array
- * @size: Size of array
- * @value: Value to find
- *
- * Return: index where value is located
+ * linear_search - function that searches for values in a sorted array
+ * @array: pointer to the first element of the array
+ * @size: is the number of elements in array
+ * @value: value to search for
+ * Return: the index where value is located
  */
+
 int linear_search(int *array, size_t size, int value)
 {
-	int index = 0;
+	size_t index = 0;
 
-	if (array == NULL)
+	if (value && array == NULL)
 		return (-1);
 
-	for (size_t i = 0; i < size; i++)
+	while (index != size)
 	{
-		printf("Value checked array[%d] = [%d]\n", index, *(array + index));
-		if (*(array + index) == value)
+		printf("Value checked array[%lu] = [%i]\n", index, array[index]);
+		if (array[index] == value)
 			return (index);
 
 		index++;
 	}
 	return (-1);
+
 }
