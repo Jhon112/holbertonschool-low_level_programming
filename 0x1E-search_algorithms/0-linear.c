4ed7cdbe8e4151013709c284850fc30c3c104f84
Jhon Arias <jhonarmont@hotmail.com>
2020-01-16 22:57:04 -0500

    Fix betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,23 +4,23 @@
  * @array: pointer to first element of array
  * @size: Size of array
  * @value: Value to find
- * 
+ *
  * Return: index where value is located
  */
 int linear_search(int *array, size_t size, int value)
 {
-    int index = 0;
+	int index = 0;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    for (size_t i = 0; i < size; i++)
-    {
-        printf("Value checked array[%d] = [%d]\n", index, *(array + index));
-        if (*(array + index) == value)
-            return (value);
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", index, *(array + index));
+		if (*(array + index) == value)
+			return (value);
 
-        index++;
-    }
-    return (-1);
+		index++;
+	}
+	return (-1);
 }
