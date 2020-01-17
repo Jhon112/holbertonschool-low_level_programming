7d09a840d179f14876e41e2b97d85bf4f50733ec
Jhon Arias <jhonarmont@hotmail.com>
2020-01-16 23:18:57 -0500

    Edit script

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,11 +10,12 @@
 int linear_search(int *array, size_t size, int value)
 {
 	int index = 0;
+	size_t i;
 
 	if (array == NULL)
 		return (-1);
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", index, *(array + index));
 		if (*(array + index) == value)
